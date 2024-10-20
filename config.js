919feb79b1baf7dd5e9d39c2d843dc8371c992d4
hahacaicac <162639989+hahacaicac@users.noreply.github.com>
2024-10-20 22:01:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'Em';
+const nameGirl = 'Thu Minh';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 20-10';
-const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const eventName = 'Chúc Mừng 20-10 Vui Zẻ Thu Minh';
+const titleCard = 'Tặng Người Tớ Iu';
+const contentCard = 'Chúc cậu 20/10 vui vẻ và ngày càng xinh đẹp nhé công chúa của a 💩 a đéo biết chúc gì nữa nhưng mà tớ yêu cậu nhiều lắm thu minh❤';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
